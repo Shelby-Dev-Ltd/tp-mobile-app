ef86cb494d7e0dbc87b04517a7a0bb6f3ab6c48d
Kennard Sugirotok <kennardfx@gmail.com>
2024-03-11 15:19:28 +0700

    feat: added loading screen for analytics screen

--- components/screens/analytics.tsx
+++ components/screens/analytics.tsx
@@ -1,15 +1,27 @@
 import { Text, View } from "react-native";
 import Layout from "../layouts/Layout";
 import { screenProps } from "../../types/screenprops";
+import { Colors, Image, LoaderScreen } from "react-native-ui-lib";
+import { useEffect, useState } from "react";
 
-export default function AnalyticscScreen({ title, navigation, openedPage }: screenProps) {
+export default function AnalyticsScreen({ title, navigation, openedPage }: screenProps) {
+    const [isLoadingContent, setIsLoadingContent] = useState<boolean>(true)
+
+    useEffect(() => {
+        setTimeout(() => setIsLoadingContent(false), 3000) // dummy loading
+    }, [])
+
     const content =
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>{title}</Text>
+            <Image source={{ uri: 'https://github.com/wix/react-native-ui-lib/blob/master/demo/src/assets/images/card-example.jpg' }} />
         </View>
 
-
     return (
-        <Layout openedPage={openedPage} content={content} title={title} navigation={navigation} />
-    );
+        <Layout
+            openedPage={openedPage}
+            content={isLoadingContent ? <LoaderScreen message={`Loading ${title}`} color={Colors.grey40} /> : content}
+            title={title}
+            navigation={navigation}
+        />
+    )
 }
