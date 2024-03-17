6ce6e986d360485d83890e62ac7c2b237def80f4
Arina Sabilahaq <arinasabilahaq11@gmail.com>
2024-03-18 00:51:40 +0700

    fix/Update navigation routes from onboarding to main APK

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+import { 
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View 
+} from 'react-native';
+
+import { 
+  NavigationContainer, 
+} from '@react-navigation/native';
 import { Onboarding } from './components/screens/Onboarding';
 import Register from './components/screens/register';
+import Login from './components/screens/Login';
+import Routes from './routes/routes';
+import BaseScreen from './components/screens/base';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-
-      <Register/>
-      {/* <Onboarding /> */}
-    </SafeAreaView>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Routes/>
+      </NavigationContainer>
+    </SafeAreaView>     
   );
 }
 
--- components/screens/base.tsx
+++ components/screens/base.tsx
@@ -1,71 +1,104 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './home';
-import HistoryScreen from './history';
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./home";
+import HistoryScreen from "./history";
 import { screenProps } from "../../types/screenprops";
-import RecordScreen from './record';
-import AnalyticscScreen from './analytics';
-import SettingsScreen from './settings';
+import RecordScreen from "./record";
+import AnalyticscScreen from "./analytics";
+import SettingsScreen from "./settings";
 
 const Stack = createNativeStackNavigator();
 
 export default function BaseScreen() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="home"
-          options={{
-            title: 'Home',
-            headerLeft: () => <></>,
-            animation: 'none',
-          }}
-        >
-          {(props: any) => <HomeScreen {...props} title={props.route.name} navigation={props.navigation} openedPage={props.route.name} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name="history"
-          options={{
-            title: 'History',
-            headerLeft: () => <></>,
-            animation: 'none',
-          }}
-        >
-          {(props: any) => <HistoryScreen {...props} title={props.route.name} navigation={props.navigation} openedPage={props.route.name} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name="record"
-          options={{
-            title: 'Record',
-            headerLeft: () => <></>,
-            animation: 'none',
-          }}
-        >
-          {(props: any) => <RecordScreen {...props} title={props.route.name} navigation={props.navigation} openedPage={props.route.name} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name="analytics"
-          options={{
-            title: 'Analytics',
-            headerLeft: () => <></>,
-            animation: 'none',
-          }}
-        >
-          {(props: any) => <AnalyticscScreen {...props} title={props.route.name} navigation={props.navigation} openedPage={props.route.name} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name="settings"
-          options={{
-            title: 'Settings',
-            headerLeft: () => <></>,
-            animation: 'none',
-          }}
-        >
-          {(props: any) => <SettingsScreen {...props} title={props.route.name} navigation={props.navigation} openedPage={props.route.name} />}
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerLeft: () => <></>,
+          animation: "none",
+        }}
+      >
+        {(props: any) => (
+          <HomeScreen
+            {...props}
+            title={props.route.name}
+            navigation={props.navigation}
+            openedPage={props.route.name}
+          />
+        )}
+      </Stack.Screen>
+      <Stack.Screen
+        name="history"
+        options={{
+          title: "History",
+          headerLeft: () => <></>,
+          animation: "none",
+        }}
+      >
+        {(props: any) => (
+          <HistoryScreen
+            {...props}
+            title={props.route.name}
+            navigation={props.navigation}
+            openedPage={props.route.name}
+          />
+        )}
+      </Stack.Screen>
+      <Stack.Screen
+        name="record"
+        options={{
+          title: "Record",
+          headerLeft: () => <></>,
+          animation: "none",
+        }}
+      >
+        {(props: any) => (
+          <RecordScreen
+            {...props}
+            title={props.route.name}
+            navigation={props.navigation}
+            openedPage={props.route.name}
+          />
+        )}
+      </Stack.Screen>
+      <Stack.Screen
+        name="analytics"
+        options={{
+          title: "Analytics",
+          headerLeft: () => <></>,
+          animation: "none",
+        }}
+      >
+        {(props: any) => (
+          <AnalyticscScreen
+            {...props}
+            title={props.route.name}
+            navigation={props.navigation}
+            openedPage={props.route.name}
+          />
+        )}
+      </Stack.Screen>
+      <Stack.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          headerLeft: () => <></>,
+          animation: "none",
+        }}
+      >
+        {(props: any) => (
+          <SettingsScreen
+            {...props}
+            title={props.route.name}
+            navigation={props.navigation}
+            openedPage={props.route.name}
+          />
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
   );
 }
