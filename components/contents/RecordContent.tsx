120934036537ff6feca70f39f97dd041a6f6facb
Kennard Sugirotok <kennardfx@gmail.com>
2024-03-24 07:35:39 +0700

    feat: added capture feature and google vision integration

--- components/contents/RecordContent.tsx
+++ components/contents/RecordContent.tsx
@@ -1,7 +1,8 @@
-import { AutoFocus, Camera } from 'expo-camera'
-import { useState } from 'react';
+import { AutoFocus, Camera, CameraCapturedPicture } from 'expo-camera'
+import { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 import { useIsFocused } from "@react-navigation/native";
+// import { useCameraDevice, Camera as VCamera } from "react-native-vision-camera"
 
 import { Text, TouchableOpacity, View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
@@ -9,7 +10,11 @@ import { Ionicons } from '@expo/vector-icons';
 export default function RecordContent() {
     let camera: Camera
     const isFocused = useIsFocused();
-    const [startCamera, setStartCamera] = useState(false)
+    // const device = useCameraDevice('back');
+    const [startCamera, setStartCamera] = useState<boolean>(false)
+    const [isUploading, setIsUploading] = useState<boolean>(false)
+    const [googleResponse, setGoogleResponse] = useState()
+    const [capturedImage, setCapturedImage] = useState<string>()
 
     const __startCamera = async () => {
         const { status } = await Camera.requestCameraPermissionsAsync()
@@ -21,13 +26,84 @@ export default function RecordContent() {
         }
     }
 
+    const CaptureImage = async () => {
+        try {
+            const image: CameraCapturedPicture = await camera.takePictureAsync()
+            const imageUri: string = image.uri
+
+            setCapturedImage(imageUri)
+        } catch (e) {
+            console.error(e)
+        } finally {
+            SubmitToGoogle()
+        }
+
+    }
+
+    const SubmitToGoogle = async () => {
+        try {
+            setIsUploading(true);
+            let body = JSON.stringify({
+                requests: [
+                    {
+                        features: [
+                            { type: "LABEL_DETECTION", maxResults: 10 },
+                            { type: "LANDMARK_DETECTION", maxResults: 5 },
+                            { type: "FACE_DETECTION", maxResults: 5 },
+                            { type: "LOGO_DETECTION", maxResults: 5 },
+                            { type: "TEXT_DETECTION", maxResults: 5 },
+                            { type: "DOCUMENT_TEXT_DETECTION", maxResults: 5 },
+                            { type: "SAFE_SEARCH_DETECTION", maxResults: 5 },
+                            { type: "IMAGE_PROPERTIES", maxResults: 5 },
+                            { type: "CROP_HINTS", maxResults: 5 },
+                            { type: "WEB_DETECTION", maxResults: 5 }
+                        ],
+                        image: {
+                            source: {
+                                imageUri: capturedImage
+                            }
+                        }
+                    }
+                ]
+            });
+            let response = await fetch(
+                "https://vision.googleapis.com/v1/images:annotate?key=" +
+                process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+                {
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    method: "POST",
+                    body: body
+                }
+            );
+            let responseJson = await response.json();
+            console.log(responseJson)
+
+            setGoogleResponse(responseJson)
+            setIsUploading(false)
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    //Logs
+    useEffect(() => {
+        console.log(`UPLOADING: ${isUploading}`)
+        console.log(`RES: ${googleResponse}`)
+    }, [isUploading, googleResponse])
+
     if (isFocused) {
+
+        // if (device == null) return <Text>No Camera Device found!</Text>
+
         return (
             <View
                 style={{ flex: 1, width: '100%', height: '100%' }}
             >
                 <TouchableOpacity
-                    onPress={() => { }}
+                    onPress={() => CaptureImage()}
                     style={{
                         width: 70,
                         borderRadius: 50,
@@ -59,6 +135,11 @@ export default function RecordContent() {
                         camera = r as Camera
                     }}
                 />
+                {/* <VCamera
+                    style={{ flex: 1, width: "auto", height: "auto" }}
+                    device={device}
+                    isActive={true}
+                /> */}
             </View>
         );
     }
