98e4e0d88bf7b21b89ce43058178499545a07be1
Bariq Maulana <95336830+BariqMaulana@users.noreply.github.com>
2024-04-27 01:48:15 +0800

    fix: fixed map view

--- components/contents/analytics/Analytics.tsx
+++ components/contents/analytics/Analytics.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Dimensions, Image, ScrollView, Text, View } from "react-native";
 import { NavigationType } from "../../../types/navigation";
 import { imagePlaceholders } from "../../../config/placeholders";
 import { BezierLineChart } from "../../charts/BezierLineChart";
@@ -9,15 +9,30 @@ import { DatasetsType } from "../../../types/chart";
 import { Dataset } from "react-native-chart-kit/dist/HelperTypes";
 import DonutChart from "../../charts/DonutChart";
 import DonutChartContainer from "./DonutChartContainer";
-import MapView from "react-native-maps";
+import MapView, {LatLng, Marker, PROVIDER_GOOGLE} from "react-native-maps";
+import { vehicleCountStyles } from "../../../styles/vehicleCount";
+import useRecords from "../../../data/records";
 
 type AnalyticsProps = {
     navigation: NavigationType
 }
 
 const Analytics = ({ navigation }: AnalyticsProps) => {
-
+    
+    const [origin, setOrigin] = useState<LatLng | null>(); //new
     const { isLoading, mutate, data: analyticsData } = useAnalytics();
+    const { width, height } = Dimensions.get("window");
+    const ASPECT_RATIO = width / (height/3);
+    const LATITUDE_DELTA = 0.02;
+    const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+    const INITIAL_LAT = -6.284512;
+    const INITIAL_LNG = 107.170716;
+    const INITIAL_POSITION = {
+        latitude: INITIAL_LAT,
+        longitude: INITIAL_LNG,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      };
 
     if (isLoading) return <LoaderScreen />;
 
@@ -28,22 +43,30 @@ const Analytics = ({ navigation }: AnalyticsProps) => {
     return (
         <ScrollView 
         style={{ flex: 1 }}>
+            <Text style={vehicleCountStyles.text}>
+                Map Address: 
+            </Text>
            <MapView
-            initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+           provider={PROVIDER_GOOGLE}
+            initialRegion={INITIAL_POSITION}
+            style={{
+                height: 200,
+                borderRadius: 20
             }}
-            pitchEnabled={false}
-            zoomEnabled={false}
+            pitchEnabled
+            zoomEnabled
             scrollEnabled={false}
             rotateEnabled={false}
-            />
+            >
+            <Marker coordinate={INITIAL_POSITION} />
+            </MapView>
+            <Text style={vehicleCountStyles.text}>
+                Donut Chart: 
+            </Text>
             <DonutChartContainer analyticsData={analyticsData}/>
             {/* <View style={{ padding: 10 }}>
                 <View style={{ marginBottom: 20 }}>
-                    <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center', marginBottom: -30 }}></Text>
+                <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center', marginBottom: -30 }}></Text>
                     <Image
                         source={{ uri: imagePlaceholders.analytics.trafficThumbnail }}
                     />
