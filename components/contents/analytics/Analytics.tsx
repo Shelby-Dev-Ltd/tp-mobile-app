edeebc789a2d9d73d1f19c147c710f2468581d56
Kennard Sugirotok <kennardfx@gmail.com>
2024-05-15 09:26:39 +0700

    fix: analytics data fetch

--- components/contents/analytics/Analytics.tsx
+++ components/contents/analytics/Analytics.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Dimensions, Image, RefreshControl, ScrollView, Text, View } from "react-native";
 import { NavigationType } from "../../../types/navigation";
 import { useAnalytics } from "../../../data/analytics";
@@ -16,15 +16,11 @@ const Analytics = ({ navigation }: AnalyticsProps) => {
 
     const { isLoading, mutate, data: analyticsData } = useAnalytics();
 
-    if (isLoading) return <LoaderScreen />;
-
-    if (!analyticsData) return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>No data found!</Text></View>
-    )
-
     const onRefresh = () => {
         mutate();
     };
+
+    if (isLoading) return <LoaderScreen />;
 
     return (
         <ScrollView
@@ -41,48 +37,51 @@ const Analytics = ({ navigation }: AnalyticsProps) => {
                 />
             }
         >
-            <View
-                style={{
-                    height: '100%',
-                    width: '100%',
-                    flexDirection: 'column',
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                }}
-            >
-                <Card
-                    marginTop={10}
-                    marginBottom={10}
-                    width={width - 40}
-                >
-                    <DonutChartContainer
-                        isLoading={isLoading}
-                        analyticsData={analyticsData}
-                    />
-                </Card>
-                {/* TODO: ADD PIE CHART */}
-                {/* <View>
-                    <Text style={{ fontSize: 16, fontWeight: 'bold', marginBottom: 10 }}>Pie Chart</Text>
-                    <PieChart
-                        data={vehicleData}
-                        width={Dimensions.get('window').width}
-                        height={220}
-                        chartConfig={{
-                            backgroundColor: Colors.white,
-                            decimalPlaces: 2,
-                            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                        }}
-                        accessor="population"
-                        backgroundColor="transparent"
-                        paddingLeft="15"
-                        absolute
-                    />
-                </View> */}
-                {/* </View> */}
-            </View>
-
-
-
+            {
+                !analyticsData || (Array.isArray(analyticsData) && !analyticsData.length) ?
+                    (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>No data found!</Text></View>)
+                    : (
+                        <View
+                            style={{
+                                height: '100%',
+                                width: '100%',
+                                flexDirection: 'column',
+                                justifyContent: 'flex-start',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Card
+                                marginTop={10}
+                                marginBottom={10}
+                                width={width - 40}
+                            >
+                                <DonutChartContainer
+                                    isLoading={isLoading}
+                                    analyticsData={analyticsData}
+                                />
+                            </Card>
+                            {/* TODO: ADD PIE CHART */}
+                            {/* <View>
+                            <Text style={{ fontSize: 16, fontWeight: 'bold', marginBottom: 10 }}>Pie Chart</Text>
+                            <PieChart
+                                data={vehicleData}
+                                width={Dimensions.get('window').width}
+                                height={220}
+                                chartConfig={{
+                                    backgroundColor: Colors.white,
+                                    decimalPlaces: 2,
+                                    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                                }}
+                                accessor="population"
+                                backgroundColor="transparent"
+                                paddingLeft="15"
+                                absolute
+                            />
+                            </View> */}
+                            {/* </View> */}
+                        </View>
+                    )
+            }
         </ScrollView >
     );
 }
